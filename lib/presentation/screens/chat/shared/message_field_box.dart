792ad544c83f29f6ef4f31834ce6d5b5f324ca46
MrSp000ky <miguelcamacho241@outlook.com>
2024-03-07 23:06:42 -0800

    Commit 7 Marzo

--- lib/presentation/screens/chat/shared/message_field_box.dart
+++ lib/presentation/screens/chat/shared/message_field_box.dart
@@ -1,10 +1,19 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
 
 class MessageFieldBox extends StatelessWidget {
   const MessageFieldBox({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final textController = TextEditingController();
+    final enfoque = FocusNode();
+
+
     final UnderlineInputBorder outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(
         color: Colors.transparent
@@ -12,15 +21,70 @@ class MessageFieldBox extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
     );
 
-    return TextFormField(
-      decoration: InputDecoration(
+
+    final decoration = InputDecoration(
+      hintText: "Ingresa tu pregunta y termina con un -> ?",
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_rounded),
-          onPressed: (){},)
-      ),
-    );
+          onPressed: (){
+            final textValue = textController.value.text;
+            print("Impresion de Boton de Envio: $textValue");
+            textController.clear();
+          },)
+      );
+
+
+
+
+    return TextFormField(
+      controller: textController,
+      focusNode: enfoque,
+      onFieldSubmitted: (value){
+        print("Se ha mandado un valor: $value");
+        textController.clear();
+        enfoque.requestFocus();
+      },
+      onTapOutside:(event) {
+        enfoque.unfocus();
+      },
+      decoration: setDecoration(
+        inputBorder: OutlineInputBorder(), 
+        onPressed: ()=> onPress(
+          textController: textController
+          )));
+  
   }
+  UnderlineInputBorder OutlineInputBorder() => UnderlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.transparent
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+
+
+    InputDecoration setDecoration(
+      {required inputBorder, required void Function() onPressed })
+      =>InputDecoration(
+        enabledBorder: inputBorder,
+        focusedBorder: inputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_rounded),
+          onPressed: onPressed,
+        )
+      );
+
+      void onPress({required TextEditingController textController}){
+            final textValue = textController.value.text;
+            print("Impresion de Boton de Envio: $textValue");
+            textController.clear();
+      }
+
+
+
+
 }
+
